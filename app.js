f59615884130a4ac8d40a676d17b7a5c734cc467
Vikas Kundu <kundu.vikas13@gmail.com>
2018-02-16 02:18:39 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require("express");
+var app =express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.use(express.static("public"));
+
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+var blogSchema = new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    date:{type:Date,default:Date.now}
+});
+
+var Blog= mongoose.model("Blog",blogSchema);
+
+// Blog.create({
+//     title:"Test blog",
+//     image:"https://cdn.pixabay.com/photo/2018/02/07/14/27/pension-3137209__340.jpg",
+//     body: "I found this pic on Pixabay"
+// });
+
+app.get("/blogs",function(req,res){
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("index",{blogs:blogs});
+        }
+    });
+});
+
+app.get("/blogs/new",function(req,res){
+    res.render("new");
+});
+
+app.post("/blogs",function(req,res){
+    
+    Blog.create(req.body.blog,function(err,newBlog){
+        if(err)
+            console.log(err);
+        else
+        {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+
+app.get("/blogs/:id",function(req, res) {
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err)
+            res.redirect("/blogs")
+        else{
+            res.render("show",{blog:foundBlog});
+        }
+        
+    });
+});
+
+app.get("/blogs/:id/edit",function(req, res) {
+   
+    res.render("edit");
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server started!!")
+});
